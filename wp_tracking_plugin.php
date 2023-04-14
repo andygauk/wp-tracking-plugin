1164c5d2e231bc9a089e5ba33fe2a19060d4b399
andygauk <andygaukrodger@gmail.com>
2023-04-14 17:45:18 +0100

    Update wp_tracking_plugin.php

--- wp_tracking_plugin.php
+++ wp_tracking_plugin.php
@@ -1,51 +1,33 @@
 <?php
-/**
- * Plugin Name: My Tracking Plugin
- * Description: Automatically append tracking parameter to external links.
- * Version: 1.0
- * Author: Andy Gaukrodger
- * License: GPL2
- */
+/*
+Plugin Name: URL Parameter Appender
+Plugin URI: https://your-plugin-url.com
+Description: Appends a URL parameter to every external link on your website.
+Version: 1.0
+Author: Your Name
+Author URI: https://your-website-url.com
+*/
 
-// Hook the function to wp_footer action
-add_action( 'wp_footer', 'my_tracking_plugin_add_tracking_parameter' );
+// Hook the function to the 'wp_footer' action, which is called before the closing </body> tag
+add_action('wp_footer', 'append_url_parameter_to_external_links');
 
-function my_tracking_plugin_add_tracking_parameter() {
-	// Check if user is logged in or if it's an admin page
-	if ( is_user_logged_in() || is_admin() ) {
-		return;
-	}
-
-	// Get the site URL
-	$site_url = site_url();
-
-	// Get all the links in the page
-	$dom = new DOMDocument();
-	@$dom->loadHTML( mb_convert_encoding( get_ob_content(), 'HTML-ENTITIES', 'UTF-8' ) );
-	$links = $dom->getElementsByTagName( 'a' );
-
-	// Loop through each link
-	foreach ( $links as $link ) {
-		$url = $link->getAttribute( 'href' );
-		// Check if the link is external
-		if ( ! empty( $url ) && ( strpos( $url, 'http://' ) === 0 || strpos( $url, 'https://' ) === 0 ) && strpos( $url, $site_url ) === false ) {
-			// Append the tracking parameter to the URL
-			$url = add_query_arg( 'utm_source', 'myplugin', $url );
-			$link->setAttribute( 'href', $url );
-		}
-	}
-
-	// Output the updated HTML
-	echo $dom->saveHTML();
+function append_url_parameter_to_external_links() {
+    // Get the current site URL
+    $site_url = site_url();
+  
+    // Get all the links in the post content
+    $content = get_the_content();
+    $links = preg_match_all('/<a[^>]+href=([\'"])(.+?)\1/is', $content, $matches);
+  
+    if ($links) {
+        foreach ($matches[2] as $link) {
+            // Check if the link is external
+            if (strpos($link, $site_url) === false) {
+                // Append the URL parameter to the external link
+                $link = add_query_arg('Source=', 'https://isitablog.com', $link);
+                echo "<script>document.querySelector('a[href=\"" . esc_url($link) . "\"]').href='" . esc_url($link) . "';</script>";
+            }
+        }
+    }
 }
-
-// Helper function to get the output buffer content
-function get_ob_content() {
-	$content = '';
-	if ( ob_get_length() > 0 ) {
-		$content = ob_get_contents();
-		ob_end_clean();
-	}
-
-	return $content;
-}
+?>
